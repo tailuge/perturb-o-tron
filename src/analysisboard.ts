adc428f2dbf65ae8efbf40353be83f7142313614
tailuge <tailuge@gmail.com>
2018-06-17 07:43:35 +0000

    use inputs

--- src/analysisboard.ts
+++ src/analysisboard.ts
@@ -14,6 +14,12 @@ export class AnalysisBoard {
 
   perturb(fen, perturbedSquare) {
     this.clear()
+    this.chessground.set({
+    fen: fen,
+    drawable: {
+      enabled: false
+    }
+  })
     new Generator(fen).perturb(perturbedSquare).forEach(p => {
       this.stockfishQueue.enqueue(p, x => {
         this.annotate(x)
@@ -27,17 +33,38 @@ export class AnalysisBoard {
   }
 
   private annotate(position) {
-    this.shapes.push({
+    this.shapes.push(this.circle(position))
+    this.shapes.push(this.piece(position))
+    this.shapes.push(this.line(position))
+    this.chessground.setShapes(this.shapes)
+  }
+
+  private circle(position) {
+    return {
       orig: position.targetSquare,
       brush: this.brush(position.score)
-    })
+    }
+  }
+
+  private line(position) {
     let bestMove = position.bestMove
-    this.shapes.push({
+    return {
       orig: bestMove.substring(0, 2),
       dest: bestMove.substring(2, 4),
       brush: this.brush(position.score)
-    })
-    this.chessground.setShapes(this.shapes)
+    }
+  }
+
+  private piece(position) {
+    return {
+      orig: position.targetSquare,
+      brush: this.brush(position.score),
+      piece: {
+        color: "white",
+        role: "king",
+        scale: 0.6
+      }
+    }
   }
 
   private brush(score) {
