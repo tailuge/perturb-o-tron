0fd2d6f7af035894a0237575dd87dccd094fe789
tailuge <tailuge@gmail.com>
2018-06-18 20:32:28 +0000

    select perturbation

--- src/analysisboard.ts
+++ src/analysisboard.ts
@@ -6,20 +6,32 @@ export class AnalysisBoard {
   private readonly stockfishQueue
   private readonly chessground
   private shapes = new Shapes()
+  private onSelect
 
   constructor(stockfish, chessground) {
     this.stockfishQueue = new StockfishQueue(stockfish, console.log)
     this.chessground = chessground
   }
 
-  perturb(fen, perturbedSquare) {
-    this.clear()
-    this.chessground.set({
+  static config(fen) {
+    return {
       fen: fen,
       drawable: {
         enabled: false
+      },
+      selectable: {
+        enabled: false
+      },
+      highlight: {
+        lastMove: false,
+        check: false
       }
-    })
+    }
+  }
+
+  perturb(fen, perturbedSquare) {
+    this.clear()
+    this.chessground.set(AnalysisBoard.config(fen))
     new Generator(fen).perturb(perturbedSquare).forEach(p => {
       this.stockfishQueue.enqueue(p, x => {
         this.annotate(x)
@@ -27,13 +39,44 @@ export class AnalysisBoard {
     })
   }
 
+  depth(depth) {
+    this.stockfishQueue.depth = depth
+  }
+
+  selectMode(onSelect) {
+    this.onSelect = onSelect
+    this.chessground.set({
+      movable: {
+        color: undefined
+      },
+      events: {
+        select: x => this.selectPerturbedPiece(x)
+      }
+    })
+  }
+
+  selectPerturbedPiece(square) {
+    console.log(square)
+    this.chessground.setShapes([
+      {
+        orig: square,
+        brush: "blue"
+      }
+    ])
+    this.chessground.set({
+      movable: {
+        color: "both"
+      },
+      events: {
+        select: undefined
+      }
+    })
+    this.onSelect(square)
+  }
+
   private clear() {
     this.shapes.clear()
     this.chessground.setShapes(this.shapes.shapes)
-  }
-
-  depth(depth) {
-    this.stockfishQueue.depth = depth
   }
 
   private annotate(position) {
